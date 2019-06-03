4ab0abb8aabc09e064ab4fc9d8aab31bf484fad3
yang <642444906@qq.com>
2019-06-03 22:02:06 +0800

    spark hello world

---
+++ src/main/java/com/my/project/spark/SimpleApp.java
@@ -0,0 +1,19 @@
+package com.my.project.spark;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+
+public class SimpleApp {
+	public static void main(String[] args) {
+		String logFile = "D:\\tools\\spark-2.4.3-bin-hadoop2.7\\README.md"; // Should be some file on your system
+		SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+		Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+		long numAs = logData.filter(s -> s.contains("a")).count();
+		long numBs = logData.filter(s -> s.contains("b")).count();
+
+		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+		spark.stop();
+	}
+}

dc3bed0e0e18f476b7dd9347da0d253b7858790e
yang <642444906@qq.com>
2019-11-29 01:14:00 +0800

    Add comment to SimpleApp

--- src/main/java/com/my/project/spark/SimpleApp.java
+++ src/main/java/com/my/project/spark/SimpleApp.java
@@ -3,6 +3,9 @@ package com.my.project.spark;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 
+/**
+ * bin/spark-submit --class "com.my.project.spark.SimpleApp" --master local[4] hello-spark-0.0.1.jar
+ */
 public class SimpleApp {
 	public static void main(String[] args) {
 		String logFile = "D:\\tools\\spark-2.4.3-bin-hadoop2.7\\README.md"; // Should be some file on your system
